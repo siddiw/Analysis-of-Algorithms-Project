52f7667cf01fb441d3910a36a2337cd293ba3fe9
Siddhi Wadgaonkar <siddhiw@Siddhis-MacBook-Pro.local>
2022-03-31 02:08:21 -0400

    TASK 3b implementation - dev test OK

--- Task3b.java
+++ Task3b.java
@@ -1,9 +1,24 @@
 public class Task3b extends Problem1 {
     @Override
     public void solve() {
-        for (int i : arrAQI) {
-            System.out.print(i + " ");
+        int maxSumSoFar = 0;
+        int currSum = 0, currStart = 0;
+
+        for (int currPtr = 0; currPtr < nsize; currPtr++) {
+            currSum += arrAQI[currPtr];
+
+            if (maxSumSoFar < currSum) {
+                maxSumSoFar = currSum;
+                l = currStart;
+                r = currPtr;
+            }
+
+            if (currSum < 0) {
+                currSum = 0;
+                currStart = currPtr + 1;
+            }
         }
-        System.out.println("");
+
+        sum = maxSumSoFar;
     }
 }

4bae58585119cd4dda04febaebdee5ddfcbfc4e8
Siddhi Wadgaonkar <siddhiw@Siddhis-MacBook-Pro.local>
2022-03-31 01:18:44 -0400

    TASK 1 implementation - dev test OK

--- Task1.java
+++ Task1.java
@@ -1,12 +1,28 @@
 public class Task1 extends Problem1 {
     @Override
     public void solve() {
-        // for (int i : arr) {
-        //     System.out.print(i + " ");
-        // }
-        // System.out.println("");
+        int maxSumSoFar = 0;
 
-        
+        // left - left pointer
+        for (int left = 0; left < nsize; left++) {
+            // right - right pointer
+            for (int right = left; right < nsize; right++) {
+                int currSum = 0;
+                for (int temp = left; temp <= right; temp++) {
+                    currSum += arrAQI[temp];
+                }
 
+                // Update if it is the max so far.
+                // Also update l and r
+                // if a new max is discovered.
+                if (maxSumSoFar < currSum) {
+                    maxSumSoFar = currSum;
+                    l = left;
+                    r = right;
+                }
+            }
+        }
+
+        sum = maxSumSoFar;
     }
 }

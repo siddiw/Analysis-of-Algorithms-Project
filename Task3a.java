972536b9a838e83a4b836e72853c7a706271ce32
Siddhi Wadgaonkar <siddhiw@Siddhis-MacBook-Pro.local>
2022-04-05 13:11:28 -0400

    Task 3a

--- Task3a.java
+++ Task3a.java
@@ -1,77 +1,56 @@
+// Refactoring
 public class Task3a extends Problem1 {
 
-    int[] dp = new int[nsize];
+    int[] dp; 
 
+    public int helper(int index)
+    {
+        if (index >= arrAQI.length)
+        {
+            return 0;
+        }
 
-    // public int helper(int index, )
-    // {
-    //     // if (idx >= arrAQI.size())
-    //     // {
-    //     //     return 0;
-    //     // }
-    //     // if (dp.count(idx))
-    //     // {
-    //     //     return dp[idx];
-    //     // }
-    //     // int curr = arrAQI[idx];
-    //     // int sum = arrAQI[idx] + solve(arrAQI, idx + 1, dp);
-    //     // return dp[idx] = max(curr, sum);
-    // }
+        if (dp[index] > 0)
+        {
+            return dp[index];
+        }
 
+        int curr = arrAQI[index];
+        int sum = arrAQI[index] + helper(index+1);
+        dp[index] = Math.max(curr, sum);
+        return dp[index];
+    }
 
     @Override
     public void solve() {
+        dp = new int[nsize+1];
+        int maxSumSoFar = Integer.MIN_VALUE, x = Integer.MIN_VALUE;
+        int s = 0 , e = 0;
 
+        for(int i = 0; i < nsize; i++){
+            x = helper(i);
+            if (x > maxSumSoFar)
+            {
+                maxSumSoFar = x;
+                s = i;
+            }
+        }
+
+        x = maxSumSoFar;
+        e = s;
+
+        while (x > 0)
+        {
+            x -= arrAQI[e];
+            if (x == 0)
+            {
+                break;
+            }
+            e++;
+        }
+
+        sum = maxSumSoFar;
+        l = s;
+        r = e;
     }
-
-    // 
-    // int solve(vector<int> &arrAQI, int idx, unordered_map<int, int> &dp)
-    // {
-    //     if (idx >= arrAQI.size())
-    //     {
-    //         return 0;
-    //     }
-    //     if (dp.count(idx))
-    //     {
-    //         return dp[idx];
-    //     }
-    //     int curr = arrAQI[idx];
-    //     int sum = arrAQI[idx] + solve(arrAQI, idx + 1, dp);
-    //     return dp[idx] = max(curr, sum);
-    // }
-    // void task3a(vector<int> arrAQI, int n, unordered_map<int, int> &dp)
-    // {
-    //     int maxSumSoFar = INT_MIN, x = INT_MIN;
-    //     int start = 0;
-    //     int end = 0;
-
-    //     for (int i = 0; i < n; i++)
-    //     {
-    //         x = solve(arrAQI, i, dp);
-    //         if (x > maxSumSoFar)
-    //         {
-    //             maxSumSoFar = x;
-    //             start = i;
-    //         }
-    //     }
-
-    //     x = maxSumSoFar;
-
-    //     end = start;
-
-    //     while (x)
-    //     {
-    //         x -= arrAQI[end];
-    //         if (x == 0)
-    //         {
-    //             break;
-    //         }
-    //         end++;
-    //     }
-
-    //     start++;
-    //     end++;
-    //     cout << start << " " << end << " " << maxSumSoFar;
-    // }
-    //
 }

0ca8c62ed7b8963f8149464a4b203587a74ca21c
Siddhi Wadgaonkar <siddhiw@ssrb-vpn2-8-190.vpn.ufl.edu>
2022-04-05 11:20:02 -0400

    TASK 2 implementation

--- Task2.java
+++ Task2.java
@@ -1,9 +1,26 @@
 public class Task2 extends Problem1 {
     @Override
     public void solve() {
-        for (int i : arrAQI) {
-            System.out.print(i + " ");
+        int maxSumSoFar = Integer.MIN_VALUE;
+        int temp = 0;
+        int[] dp = new int[nsize + 1];
+
+        dp[0] = 0;
+
+        for (int i = 1; i <= nsize; i++) {
+            dp[i] = dp[i - 1] + arrAQI[i - 1];
         }
-        System.out.println("");
+
+        for (int left = 1; left <= nsize; left++) {
+            for (int right = left; right <= nsize; right++) {
+                temp = dp[right] - dp[left - 1];
+                if (temp > maxSumSoFar) {
+                    maxSumSoFar = temp;
+                    l = left - 1;
+                    r = right - 1;
+                }
+            }
+        }
+        sum = maxSumSoFar;
     }
 }
